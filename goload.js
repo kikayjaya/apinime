eab394332f76fff218fd9c532ddfb494eb007e93
kikayjaya <106851804+kikayjaya@users.noreply.github.com>
2022-09-14 07:34:44 +0700

    Update goload.js

--- goload.js
+++ goload.js
@@ -1,21 +1,9 @@
-const axios =  require('axios');
-const CryptoJS = require('crypto-js');
+import CryptoJS from 'crypto-js';
 
-const ENCRYPTION_KEYS_URL =
-    'https://raw.githubusercontent.com/justfoolingaround/animdl-provider-benchmarks/master/api/gogoanime.json';
-
-let iv = null;
-let key = null;
-let second_key = null;
-
-const fetch_keys = async() => {
-    const response = await axios.get(ENCRYPTION_KEYS_URL);
-    const res = response.data;
-    return {
-        iv: CryptoJS.enc.Utf8.parse(res.iv),
-        key: CryptoJS.enc.Utf8.parse(res.key),
-        second_key: CryptoJS.enc.Utf8.parse(res.second_key),
-    };
+const keys = {
+    key: CryptoJS.enc.Utf8.parse('37911490979715163134003223491201'),
+    second_key: CryptoJS.enc.Utf8.parse('54674138327930866480207815084989'),
+    iv: CryptoJS.enc.Utf8.parse('3134003223491201'),
 };
 
 /**
@@ -23,20 +11,15 @@ const fetch_keys = async() => {
  * @param {cheerio} $ Cheerio object of the embedded video page
  * @param {string} id Id of the embedded video URL
  */
-async function generateEncryptAjaxParameters($, id) {
-    const keys = await fetch_keys();
-    iv = keys.iv;
-    key = keys.key;
-    second_key = keys.second_key;
-
+export async function generateEncryptAjaxParameters($, id) {
     // encrypt the key
-    const encrypted_key = CryptoJS.AES['encrypt'](id, key, {
-        iv: iv,
+    const encrypted_key = CryptoJS.AES['encrypt'](id, keys.key, {
+        iv: keys.iv,
     });
 
     const script = $("script[data-name='episode']").data().value;
-    const token = CryptoJS.AES['decrypt'](script, key, {
-        iv: iv,
+    const token = CryptoJS.AES['decrypt'](script, keys.key, {
+        iv: keys.iv,
     }).toString(CryptoJS.enc.Utf8);
 
     return 'id=' + encrypted_key + '&alias=' + id + '&' + token;
@@ -45,16 +28,11 @@ async function generateEncryptAjaxParameters($, id) {
  * Decrypts the encrypted-ajax.php response
  * @param {object} obj Response from the server
  */
-function decryptEncryptAjaxResponse(obj) {
+export function decryptEncryptAjaxResponse(obj) {
     const decrypted = CryptoJS.enc.Utf8.stringify(
-        CryptoJS.AES.decrypt(obj.data, second_key, {
-            iv: iv,
+        CryptoJS.AES.decrypt(obj.data, keys.second_key, {
+            iv: keys.iv,
         })
     );
     return JSON.parse(decrypted);
-}
-
-module.exports = {
-    generateEncryptAjaxParameters,
-    decryptEncryptAjaxResponse
 }
